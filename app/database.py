d09af14e4fe03429dccb86777b22aad004136b39
safna sulfi <safnasulfi002@gmail.com>
2026-02-01 13:07:43 +0530

    debug

--- app/database.py
+++ app/database.py
@@ -7,6 +7,9 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is missing! Add it in Render dashboard.")
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
