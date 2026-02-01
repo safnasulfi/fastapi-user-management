9f3f9a8ecfa6a878f1cd1cc560493d845c13a158
safna sulfi <safnasulfi002@gmail.com>
2026-02-01 11:54:50 +0530

    added gitignore file

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql://postgres:123@localhost:5432/fastapi_demo"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
